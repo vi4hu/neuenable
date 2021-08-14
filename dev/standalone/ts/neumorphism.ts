c353551a3b99893686561a1044247734e9c3ab25
Ashutosh Vishu Yadav <rainkun3@protonmail.com>
2021-08-15 02:31:55 +0530

    NPM and gradient classesV0.3.0 (#11)
    * removed junk file (#6)
    
    * img exception handled -> closes GH-7
    
    * new: gradient clases
    
    * refractered the structure (#9)
    
    * V0.3.3 (#10)
    
    * adding initial npm package
    
    * some changes in scss
    
    * lock file and initial examples
    
    * minute

---
+++ dev/standalone/ts/neumorphism.ts
@@ -0,0 +1,9 @@
+import { colorLuminance } from "./manipulator"
+
+console.log(colorLuminance('#272727', .4 * -1))
+console.log(colorLuminance('#272727', .4))
+
+
+console.log(colorLuminance('#272727',0.07))
+
+console.log(colorLuminance('#272727', -0.1))
